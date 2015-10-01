0241297423b459f5b69f57a62b632c4bc296fa2b
Sebastiaan van Zelst <s.j.v.zelst@tue.nl>
2015-10-01 12:32:08 +0200

    move child contextcreation to ProMPluginContextManager

--- RapidProM/src/org/rapidprom/external/connectors/prom/ProMPluginContextManager.java
+++ RapidProM/src/org/rapidprom/external/connectors/prom/ProMPluginContextManager.java
@@ -1,6 +1,8 @@
 package org.rapidprom.external.connectors.prom;
 
 import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,8 +13,12 @@ import org.processmining.framework.boot.Boot;
 import org.processmining.framework.boot.Boot.Level;
 import org.processmining.framework.packages.PackageDescriptor;
 import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.PluginExecutionResult;
 import org.processmining.framework.plugin.PluginManager;
+import org.processmining.framework.plugin.ProMFuture;
 import org.processmining.framework.plugin.annotations.Bootable;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.impl.PluginExecutionResultImpl;
 import org.processmining.framework.plugin.impl.PluginManagerImpl;
 import org.processmining.framework.util.CommandLineArgumentList;
 import org.processmining.framework.util.PathHacker;
@@ -201,6 +207,80 @@ public class ProMPluginContextManager extends ProgressThread {
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
+	}
+
+	/**
+	 * This method prepares a PluginContext object, which is a child object of
+	 * the PluginContext provided by the "PluginContextManager". Basically this
+	 * method mimics some of the internal workings of the ProM framework, e.g.
+	 * setting the future result objects.
+	 * 
+	 * @param classContainingProMPlugin
+	 *            the class that contains the ProM plugin code
+	 * @return
+	 */
+	public PluginContext createChildContextForPluginClass(
+			Class<?> classContainingProMPlugin) {
+		final PluginContext result = ProMPluginContextManager
+				.instance()
+				.getContext()
+				.createChildContext(
+						"RapidProMPluginContext_" + System.currentTimeMillis());
+		Plugin pluginAnn = findAnnotation(
+				classContainingProMPlugin.getAnnotations(), Plugin.class);
+
+		PluginExecutionResult per = new PluginExecutionResultImpl(
+				pluginAnn.returnTypes(), pluginAnn.returnLabels(),
+				PluginManagerImpl.getInstance().getPlugin(
+						classContainingProMPlugin.getCanonicalName()));
+		ProMFuture<?>[] futures = createProMFutures(pluginAnn);
+		Method m;
+		try {
+			m = PluginExecutionResultImpl.class.getDeclaredMethod("setResult",
+					Object[].class);
+			m.setAccessible(true);
+			m.invoke(per, new Object[] { futures });
+			result.setFuture(per);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private ProMFuture<?>[] createProMFutures(Plugin pluginAnn) {
+		ProMFuture<?>[] futures = new ProMFuture<?>[pluginAnn.returnTypes().length];
+		for (int i = 0; i < pluginAnn.returnTypes().length; i++) {
+			futures[i] = new ProMFuture<Object>(pluginAnn.returnTypes()[i],
+					pluginAnn.returnLabels()[i]) {
+				@Override
+				protected Object doInBackground() throws Exception {
+					// NOP
+					return null;
+				}
+			};
+		}
+		return futures;
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends Annotation> T findAnnotation(Annotation[] annotations,
+			Class<T> clazz) {
+		T result = null;
+		for (Annotation a : annotations) {
+			if (a.annotationType().equals(clazz)) {
+				result = (T) a;
+				break;
+			}
+		}
+		return result;
 	}
 
 }
