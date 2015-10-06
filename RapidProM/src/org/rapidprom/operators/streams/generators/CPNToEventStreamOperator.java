e2dabcad7a03905a0feb158f946c363e111be555
Sebastiaan van Zelst <s.j.v.zelst@tue.nl>
2015-10-06 17:20:38 +0200

    integrating parameters of cpn based stream generator.

--- RapidProM/src/org/rapidprom/operators/streams/generators/CPNToEventStreamOperator.java
+++ RapidProM/src/org/rapidprom/operators/streams/generators/CPNToEventStreamOperator.java
@@ -20,9 +20,19 @@ import com.rapidminer.operator.ports.InputPort;
 import com.rapidminer.operator.ports.OutputPort;
 import com.rapidminer.operator.ports.metadata.GenerateNewMDRule;
 import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeInt;
 import com.rapidminer.tools.LogService;
 
 public class CPNToEventStreamOperator extends Operator {
+
+	private static String PARAMETER_KEY_MAX_STEPS = "max_steps";
+	private static String PARAMETER_LABEL_MAX_STEPS = "Max steps (-1 for no limit)";
+
+	private static String PARAMETER_KEY_REPETITIONS = "repetitions";
+	private static String PARAMETER_LABEL_REPETITIONS = "Repetitions";
+
+	private static String PARAMETER_KEY_STEP_DELAY = "step_delay";
+	private static String PARAMETER_LABEL_STEP_DELAY = "Step delay (ms)";
 
 	private InputPort inputCPNModel = getInputPorts()
 			.createPort("model (ProM CPN model)", CPNModelIOObject.class);
@@ -66,11 +76,38 @@ public class CPNToEventStreamOperator extends Operator {
 
 		logService.log("end do work Stream Generator", LogService.NOTE);
 	}
-	
+
 	@Override
 	public List<ParameterType> getParameterTypes() {
-		
+		List<ParameterType> parameterTypes = super.getParameterTypes();
+		parameterTypes.add(setupMaxStepsParameter());
+		parameterTypes.add(setupRepetitionsParameter());
+		parameterTypes.add(setupStepDelayParameter());
+		return parameterTypes;
 	}
+
+	private ParameterTypeInt setupMaxStepsParameter() {
+		ParameterTypeInt maxSteps = new ParameterTypeInt(
+				PARAMETER_KEY_MAX_STEPS, PARAMETER_LABEL_MAX_STEPS, -1,
+				Integer.MAX_VALUE, -1, true);
+		maxSteps.setOptional(false);
+		return maxSteps;
+	}
+
+	private ParameterTypeInt setupRepetitionsParameter() {
+		ParameterTypeInt repetitions = new ParameterTypeInt(
+				PARAMETER_KEY_REPETITIONS, PARAMETER_LABEL_REPETITIONS, 1,
+				Integer.MAX_VALUE, 1, true);
+		repetitions.setOptional(false);
+		return repetitions;
+	}
+
+	private ParameterTypeInt setupStepDelayParameter() {
+		ParameterTypeInt stepDelay = new ParameterTypeInt(
+				PARAMETER_KEY_STEP_DELAY, PARAMETER_LABEL_STEP_DELAY, 0,
+				Integer.MAX_VALUE, 0, true);
+		stepDelay.setOptional(false);
+		return stepDelay;
 	}
 
 }
