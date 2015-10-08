95d6d7cd59fc68c894fd1fd8826f9cd53c155cdd
Sebastiaan van Zelst <s.j.v.zelst@tue.nl>
2015-10-08 16:05:21 +0200

    add streaminductiveminer to ivy file fix deprecated stuff from cpn2stream operator

--- RapidProM/src/org/rapidprom/operators/streams/generators/CPNToEventStreamOperator.java
+++ RapidProM/src/org/rapidprom/operators/streams/generators/CPNToEventStreamOperator.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.processmining.eventstream.authors.cpn.parameters.CPN2XSStreamCaseIdentification;
-import org.processmining.eventstream.authors.cpn.parameters.CPN2XSStreamParameters;
+import org.processmining.eventstream.authors.cpn.parameters.CPN2XSEventStreamCaseIdentification;
+import org.processmining.eventstream.authors.cpn.parameters.CPN2XSEventStreamParameters;
 import org.processmining.eventstream.authors.cpn.plugins.CPNModelToXSEventStreamAuthorPlugin;
 import org.processmining.eventstream.core.interfaces.XSEventStream;
 import org.processmining.framework.plugin.PluginContext;
@@ -34,45 +34,40 @@ import com.rapidminer.tools.LogService;
 
 public class CPNToEventStreamOperator extends Operator {
 
-	/**
-	 * Parameter definitions; For categories (single selection lists) we can not
-	 * use enums as the operator can only select the index of the selected
-	 * string.
-	 */
+	private static final String PARAMETER_DEFAULT_VALUE_CASE_IDENTIFICATION_VARIABLE = CPN2XSEventStreamCaseIdentification.CPN_VARIABLE
+			.getDefaultValue();
+	private static final String PARAMETER_KEY_CASE_IDENTIFICATION = "case_identification";
+
+	private static final String PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE = "cpn_variable";
+	private static final String PARAMETER_KEY_COMMUNICATION_TYPE = "communication_type";
+
+	private static final String PARAMETER_KEY_INCLUDE_ADDITIONAL_DATA = "include_other_variables";
+
 	private static final String PARAMETER_KEY_MAX_STEPS = "max_steps";
-	private static final String PARAMETER_LABEL_MAX_STEPS = "Max steps (-1 for no limit) within one simulation of the CPN model.";
 
 	private static final String PARAMETER_KEY_REPETITIONS = "repetitions";
+	private static final String PARAMETER_KEY_STEP_DELAY = "step_delay";
+	private static final String PARAMETER_LABEL_CASE_IDENTIFICATION = "Case identification, specifying what how to identify cases within the stream.";
+	private static final String PARAMETER_LABEL_CASE_IDENTIFICATION_VARIABLE = "Case identification by CPN variable, denotes what CPN variable to track.";
+	private static final String PARAMETER_LABEL_COMMUNIATION_TYPE = "The communicationtype of the selected stream, in synchronous mode, events will only be emitted if the receiving end is ready to accept new packages.";
+
+	private static final String PARAMETER_LABEL_INCLUDE_ADDITIONAL_DATA = "If this parameter is set to true, the events will include other variable values as well";
+	private static final String PARAMETER_LABEL_MAX_STEPS = "Max steps (-1 for no limit) within one simulation of the CPN model.";
 	private static final String PARAMETER_LABEL_REPETITIONS = "Number of simulations.";
 
-	private static final String PARAMETER_KEY_STEP_DELAY = "step_delay";
 	private static final String PARAMETER_LABEL_STEP_DELAY = "Step delay (ms) inbetween two consecutive emissions.";
+	private static final String PARAMETER_OPTION_CASE_IDENTIFICATION_REPETITION = CPN2XSEventStreamCaseIdentification.REPITITION
+			.toString();
 
-	/**
-	 * TODO: bind these to CPN2XSStreamCaseIdentification, when these classes
-	 * allow for "toString()" methods.
-	 */
-	private static final String PARAMETER_KEY_CASE_IDENTIFICATION = "case_identification";
-	private static final String PARAMETER_LABEL_CASE_IDENTIFICATION = "Case identification, specifying what how to identify cases within the stream.";
-	private static final String PARAMETER_OPTION_CASE_IDENTIFICATION_REPETITION = "by repetition";
-	private static final String PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE = "by CPN variable";
+	private static final String PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE = CPN2XSEventStreamCaseIdentification.CPN_VARIABLE
+			.toString();
+	private static final String PARAMETER_OPTION_COMMUNICATION_TYPE_ASYNC = CommunicationType.ASYNC
+			.toString();
+	private static final String PARAMETER_OPTION_COMMUNICATION_TYPE_SYNC = CommunicationType.SYNC
+			.toString();
 	private static final String[] PARAMETER_OPTIONS_CASE_IDENTIFICATION = new String[] {
 			PARAMETER_OPTION_CASE_IDENTIFICATION_REPETITION,
 			PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE };
-
-	private static final String PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE = "cpn_variable";
-	private static final String PARAMETER_LABEL_CASE_IDENTIFICATION_VARIABLE = "Case identification by CPN variable, denotes what CPN variable to track.";
-	private static final String PARAMETER_DEFAULT_VALUE_CASE_IDENTIFICATION_VARIABLE = CPN2XSStreamCaseIdentification.CPN_VARIABLE_DEFAULT_VALUE;
-
-	private static final String PARAMETER_KEY_INCLUDE_ADDITIONAL_DATA = "include_other_variables";
-	private static final String PARAMETER_LABEL_INCLUDE_ADDITIONAL_DATA = "If this parameter is set to true, the events will include other variable values as well";
-
-	// TODO: bind to toString() methods of correspdoning enums in (event)stream
-	// package.
-	private static final String PARAMETER_KEY_COMMUNICATION_TYPE = "communication_type";
-	private static final String PARAMETER_LABEL_COMMUNIATION_TYPE = "The communicationtype of the selected stream, in synchronous mode, events will only be emitted if the receiving end is ready to accept new packages.";
-	private static final String PARAMETER_OPTION_COMMUNICATION_TYPE_ASYNC = "asynchronous";
-	private static final String PARAMETER_OPTION_COMMUNICATION_TYPE_SYNC = "synchronous";
 	private static final String[] PARAMETER_OPTIONS_COMMUNICATION_TYPE = new String[] {
 			PARAMETER_OPTION_COMMUNICATION_TYPE_ASYNC,
 			PARAMETER_OPTION_COMMUNICATION_TYPE_SYNC };
@@ -95,6 +90,38 @@ public class CPNToEventStreamOperator extends Operator {
 		// TODO Auto-generated constructor stub
 	}
 
+	private CPN2XSEventStreamParameters determineCaseIdentification(
+			CPN2XSEventStreamParameters params) throws UndefinedParameterError {
+		String caseIdentificationType = PARAMETER_OPTIONS_CASE_IDENTIFICATION[getParameterAsInt(
+				PARAMETER_KEY_CASE_IDENTIFICATION)];
+		if (caseIdentificationType
+				.equals(PARAMETER_OPTION_CASE_IDENTIFICATION_REPETITION)) {
+			params.setCaseIdentificationType(
+					CPN2XSEventStreamCaseIdentification.REPITITION);
+		} else if (caseIdentificationType
+				.equals(PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE)) {
+			params.setCaseIdentificationType(
+					CPN2XSEventStreamCaseIdentification.CPN_VARIABLE);
+			params.setCaseIdentifier(getParameterAsString(
+					PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE));
+		}
+		return params;
+	}
+
+	private CPN2XSEventStreamParameters determineCommunicationType(
+			CPN2XSEventStreamParameters params) throws UndefinedParameterError {
+		String communicationType = PARAMETER_OPTIONS_COMMUNICATION_TYPE[getParameterAsInt(
+				PARAMETER_KEY_COMMUNICATION_TYPE)];
+		if (communicationType
+				.equals(PARAMETER_OPTION_COMMUNICATION_TYPE_ASYNC)) {
+			params.setCommunicationType(CommunicationType.ASYNC);
+		} else if (communicationType
+				.equals(PARAMETER_OPTION_COMMUNICATION_TYPE_SYNC)) {
+			params.setCommunicationType(CommunicationType.SYNC);
+		}
+		return params;
+	}
+
 	public void doWork() throws OperatorException {
 
 		Logger logger = LogService.getRoot();
@@ -105,7 +132,7 @@ public class CPNToEventStreamOperator extends Operator {
 		PluginContext pluginContext = ProMPluginContextManager.instance()
 				.getContext();
 
-		CPN2XSStreamParameters parameters = getStreamParameters();
+		CPN2XSEventStreamParameters parameters = getStreamParameters();
 
 		Object[] result = CPNModelToXSEventStreamAuthorPlugin
 				.cpnToXSEventStreamPlugin(pluginContext,
@@ -120,9 +147,21 @@ public class CPNToEventStreamOperator extends Operator {
 		logger.log(Level.INFO, "start do work Stream Generator");
 	}
 
-	private CPN2XSStreamParameters getStreamParameters()
+	@Override
+	public List<ParameterType> getParameterTypes() {
+		List<ParameterType> parameterTypes = super.getParameterTypes();
+		parameterTypes = setupMaxStepsParameter(parameterTypes);
+		parameterTypes = setupRepetitionsParameter(parameterTypes);
+		parameterTypes = setupStepDelayParameter(parameterTypes);
+		parameterTypes = setupCaseIdentificationParameter(parameterTypes);
+		parameterTypes = setupAdditionalVariables(parameterTypes);
+		parameterTypes = setupCommunicationType(parameterTypes);
+		return parameterTypes;
+	}
+
+	private CPN2XSEventStreamParameters getStreamParameters()
 			throws UndefinedParameterError {
-		CPN2XSStreamParameters streamParams = new CPN2XSStreamParameters();
+		CPN2XSEventStreamParameters streamParams = new CPN2XSEventStreamParameters();
 		streamParams.setMaximumNumberOfStepsPerRepetition(
 				getParameterAsInt(PARAMETER_KEY_MAX_STEPS));
 		streamParams.setTotalNumberOfRepetitions(
@@ -139,47 +178,50 @@ public class CPNToEventStreamOperator extends Operator {
 		return streamParams;
 	}
 
-	private CPN2XSStreamParameters determineCaseIdentification(
-			CPN2XSStreamParameters params) throws UndefinedParameterError {
-		String caseIdentificationType = PARAMETER_OPTIONS_CASE_IDENTIFICATION[getParameterAsInt(
-				PARAMETER_KEY_CASE_IDENTIFICATION)];
-		if (caseIdentificationType
-				.equals(PARAMETER_OPTION_CASE_IDENTIFICATION_REPETITION)) {
-			params.setCaseIdentificationType(
-					CPN2XSStreamCaseIdentification.REPITITION);
-		} else if (caseIdentificationType
-				.equals(PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE)) {
-			params.setCaseIdentificationType(
-					CPN2XSStreamCaseIdentification.CPN_VARIABLE);
-			params.setCaseIdentifier(getParameterAsString(
-					PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE));
-		}
-		return params;
+	private List<ParameterType> setupAdditionalVariables(
+			List<ParameterType> parameterTypes) {
+		ParameterTypeBoolean includeVariablesParam = new ParameterTypeBoolean(
+				PARAMETER_KEY_INCLUDE_ADDITIONAL_DATA,
+				PARAMETER_LABEL_INCLUDE_ADDITIONAL_DATA, false);
+		includeVariablesParam.setOptional(false);
+		includeVariablesParam.setExpert(true);
+		parameterTypes.add(includeVariablesParam);
+		return parameterTypes;
 	}
 
-	private CPN2XSStreamParameters determineCommunicationType(
-			CPN2XSStreamParameters params) throws UndefinedParameterError {
-		String communicationType = PARAMETER_OPTIONS_COMMUNICATION_TYPE[getParameterAsInt(
-				PARAMETER_KEY_COMMUNICATION_TYPE)];
-		if (communicationType
-				.equals(PARAMETER_OPTION_COMMUNICATION_TYPE_ASYNC)) {
-			params.setCommunicationType(CommunicationType.ASYNC);
-		} else if (communicationType
-				.equals(PARAMETER_OPTION_COMMUNICATION_TYPE_SYNC)) {
-			params.setCommunicationType(CommunicationType.SYNC);
-		}
-		return params;
+	private List<ParameterType> setupCaseIdentificationParameter(
+			List<ParameterType> parameterTypes) {
+		ParameterTypeCategory caseIdentificationCat = new ParameterTypeCategory(
+				PARAMETER_KEY_CASE_IDENTIFICATION,
+				PARAMETER_LABEL_CASE_IDENTIFICATION,
+				PARAMETER_OPTIONS_CASE_IDENTIFICATION, 0, true);
+
+		parameterTypes.add(caseIdentificationCat);
+
+		ParameterTypeString caseIdentificationVariable = new ParameterTypeString(
+				PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE,
+				PARAMETER_LABEL_CASE_IDENTIFICATION_VARIABLE,
+				new String(
+						PARAMETER_DEFAULT_VALUE_CASE_IDENTIFICATION_VARIABLE),
+				true);
+		caseIdentificationVariable.setOptional(true);
+		caseIdentificationVariable
+				.registerDependencyCondition(new EqualStringCondition(this,
+						PARAMETER_KEY_CASE_IDENTIFICATION, true, new String[] {
+								PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE }));
+		parameterTypes.add(caseIdentificationVariable);
+		return parameterTypes;
 	}
 
-	@Override
-	public List<ParameterType> getParameterTypes() {
-		List<ParameterType> parameterTypes = super.getParameterTypes();
-		parameterTypes = setupMaxStepsParameter(parameterTypes);
-		parameterTypes = setupRepetitionsParameter(parameterTypes);
-		parameterTypes = setupStepDelayParameter(parameterTypes);
-		parameterTypes = setupCaseIdentificationParameter(parameterTypes);
-		parameterTypes = setupAdditionalVariables(parameterTypes);
-		parameterTypes = setupCommunicationType(parameterTypes);
+	private List<ParameterType> setupCommunicationType(
+			List<ParameterType> parameterTypes) {
+		ParameterTypeCategory communicationTypeParam = new ParameterTypeCategory(
+				PARAMETER_KEY_COMMUNICATION_TYPE,
+				PARAMETER_LABEL_COMMUNIATION_TYPE,
+				PARAMETER_OPTIONS_COMMUNICATION_TYPE, 0);
+		communicationTypeParam.setOptional(false);
+		communicationTypeParam.setExpert(true);
+		parameterTypes.add(communicationTypeParam);
 		return parameterTypes;
 	}
 
@@ -210,53 +252,6 @@ public class CPNToEventStreamOperator extends Operator {
 				Integer.MAX_VALUE, 0, true);
 		stepDelay.setOptional(false);
 		parameterTypes.add(stepDelay);
-		return parameterTypes;
-	}
-
-	private List<ParameterType> setupCaseIdentificationParameter(
-			List<ParameterType> parameterTypes) {
-		ParameterTypeCategory caseIdentificationCat = new ParameterTypeCategory(
-				PARAMETER_KEY_CASE_IDENTIFICATION,
-				PARAMETER_LABEL_CASE_IDENTIFICATION,
-				PARAMETER_OPTIONS_CASE_IDENTIFICATION, 0, true);
-
-		parameterTypes.add(caseIdentificationCat);
-
-		ParameterTypeString caseIdentificationVariable = new ParameterTypeString(
-				PARAMETER_KEY_CASE_IDENTIFICATION_VARIABLE,
-				PARAMETER_LABEL_CASE_IDENTIFICATION_VARIABLE,
-				new String(
-						PARAMETER_DEFAULT_VALUE_CASE_IDENTIFICATION_VARIABLE),
-				true);
-		caseIdentificationVariable.setOptional(true);
-		caseIdentificationVariable
-				.registerDependencyCondition(new EqualStringCondition(this,
-						PARAMETER_KEY_CASE_IDENTIFICATION, true, new String[] {
-								PARAMETER_OPTION_CASE_IDENTIFICATION_VARIABELE }));
-		parameterTypes.add(caseIdentificationVariable);
-		return parameterTypes;
-	}
-
-	private List<ParameterType> setupAdditionalVariables(
-			List<ParameterType> parameterTypes) {
-		ParameterTypeBoolean includeVariablesParam = new ParameterTypeBoolean(
-				PARAMETER_KEY_INCLUDE_ADDITIONAL_DATA,
-				PARAMETER_LABEL_INCLUDE_ADDITIONAL_DATA, false);
-		includeVariablesParam.setOptional(false);
-		includeVariablesParam.setExpert(true);
-		parameterTypes.add(includeVariablesParam);
-		return parameterTypes;
-	}
-
-	private List<ParameterType> setupCommunicationType(
-			List<ParameterType> parameterTypes) {
-		ParameterTypeCategory communicationTypeParam = new ParameterTypeCategory(
-				PARAMETER_KEY_COMMUNICATION_TYPE,
-				PARAMETER_LABEL_COMMUNIATION_TYPE,
-				PARAMETER_OPTIONS_COMMUNICATION_TYPE, 0);
-		communicationTypeParam.setOptional(false);
-		communicationTypeParam.setExpert(true);
-		parameterTypes.add(communicationTypeParam);
 		return parameterTypes;
 	}
 
