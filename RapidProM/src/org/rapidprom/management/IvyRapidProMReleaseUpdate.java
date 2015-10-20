ab2c413ac9c9f79833ffcdb2e78575c74073c04c
Sebastiaan van Zelst <s.j.v.zelst@tue.nl>
2015-10-20 14:23:40 +0200

    make recurse private

--- RapidProM/src/org/rapidprom/management/IvyRapidProMReleaseUpdate.java
+++ RapidProM/src/org/rapidprom/management/IvyRapidProMReleaseUpdate.java
@@ -43,7 +43,7 @@ public class IvyRapidProMReleaseUpdate {
 
 	}
 
-	public void recurse(File f) {
+	private void recurse(File f) {
 		for (File child : f.listFiles()) {
 			if (child.isDirectory() && !(child.getName().equals(GIT_FOLDER_NAME)
 					|| child.getName().equals(THIRDPARTY_FOLDER_NAME))) {
