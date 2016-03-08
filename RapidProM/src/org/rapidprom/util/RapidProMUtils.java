77129d63ebeb1cbbe62e52741723413664b1c9b4
Alfredo Bolt <a.bolt@tue.nl>
2016-03-08 19:40:53 +0100

    static methods to make sure that the lpsolve library is loaded

--- RapidProM/src/org/rapidprom/util/RapidProMUtils.java
+++ RapidProM/src/org/rapidprom/util/RapidProMUtils.java
@@ -5,6 +5,24 @@ import org.rapidprom.properties.RapidProMProperties;
 import com.rapidminer.tools.plugin.Plugin;
 
 public class RapidProMUtils {
+	
+	/**
+	* This static code is called to make sure that the LpSolve library gets loaded.
+	*/
+
+	static private boolean lpSolveLoaded = false;
+
+	static {
+		if (!lpSolveLoaded) {
+			init();
+		}
+	}
+
+	public static void init() {
+		System.loadLibrary("lpsolve55");
+		System.loadLibrary("lpsolve55j");
+		lpSolveLoaded = true;
+	}
 
 	public static Plugin getRapidProMPlugin() {
 		for (Plugin plugin : Plugin.getAllPlugins()) {
